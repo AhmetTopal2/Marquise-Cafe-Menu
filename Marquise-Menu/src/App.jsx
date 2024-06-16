be3a480dd298f9837b9bcd82669f8df13fd7f61b
AhmetTopal2 <168731986+AhmetTopal2@users.noreply.github.com>
2024-06-16 21:13:46 +0300

    Dosya yapisi olusturuldu

--- Marquise-Menu/src/App.jsx
+++ Marquise-Menu/src/App.jsx
@@ -1,11 +1,23 @@
 
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min'
 import './App.css'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Contact from './pages/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <></>
+    <Switch>
+      <Route path='/'>
+        <Home />
+      </Route>
+      <Route path='/menu'>
+        <Menu />
+      </Route>
+      <Route path='/contact'>
+        <Contact />
+      </Route>
+    </Switch>
   )
 }
 
